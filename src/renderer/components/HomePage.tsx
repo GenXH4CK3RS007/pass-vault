7451695c0c8676090d66d25d2f3ad3270a905174
Rohit K Manoj <rohitkmanoj@outlook.com>
2024-07-26 15:12:29 +0530

    Added tailwindcss and fonts

---
+++ src/renderer/components/HomePage.tsx
@@ -0,0 +1,7 @@
+export default function HomePage() {
+  return (
+    <div>
+      <p className="font-emph">Home</p>
+    </div>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ['./src/renderer/**/*.{js,jsx,ts,tsx,ejs}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      sans: ['Work Sans', 'sans-serif'],
+      emph: ['Rubik Mono One'],
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
